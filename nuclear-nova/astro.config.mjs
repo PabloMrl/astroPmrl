85b50155d6c10e514427195a83a8374a5ab4b550
Pablo <pabloc11b@gmail.com>
2024-03-02 22:20:25 -0300

    realice cambios en esta mierda

--- nuclear-nova/astro.config.mjs
+++ nuclear-nova/astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
-import vercel from '@astrojs/vercel/serverless';
 
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
-  output: 'server',
-  adapter: vercel(),
+  integrations: [preact(), tailwind()]
 });
